cc978e091a634e4e50c73119db17d82f336292c3
MolluskARK <molluskark@gmail.com>
2023-11-29 23:25:19 -0700

    Permissions: Improve Permissions.dll exports (#11)
    * Permissions: Remove AsaApi function exports
    
    Permissions uses the ARK_EXPORTS and ARK_API defines in the same way as
    the AsaApi headers. This results in Permissions.dll exporting functions
    from the included AsaApi headers. This change replaces ARK_EXPORTS and
    ARK_API in the Permissions code with PERMISSIONS_EXPORTS and
    PERMISSIONS_API so that Permissions.dll only exports Permissions
    functions.
    
    * Permissions: Export AddPlayerPermissionCallback()
    
    AddPlayerPermissionCallback() is declared as an API function in
    Permissions.h. However, the declaration does not match the function
    definition, resulting in the function not being exported by the DLL.
    This change fixes the declaration to match the definition.

--- Permissions/Permissions/Public/Permissions.h
+++ Permissions/Permissions/Public/Permissions.h
@@ -2,46 +2,46 @@
 
 #include <API/Ark/Ark.h>
 
-#ifdef ARK_EXPORTS
-#define ARK_API __declspec(dllexport)
+#ifdef PERMISSIONS_EXPORTS
+#define PERMISSIONS_API __declspec(dllexport)
 #else
-#define ARK_API __declspec(dllimport)
+#define PERMISSIONS_API __declspec(dllimport)
 #endif
 
 namespace Permissions
 {
-	ARK_API TArray<FString> GetPlayerGroups(const FString& eos_id);
-	ARK_API TArray<FString> GetGroupPermissions(const FString& group);
-	ARK_API TArray<FString> GetGroupMembers(const FString& group);
+	PERMISSIONS_API TArray<FString> GetPlayerGroups(const FString& eos_id);
+	PERMISSIONS_API TArray<FString> GetGroupPermissions(const FString& group);
+	PERMISSIONS_API TArray<FString> GetGroupMembers(const FString& group);
 
-	ARK_API bool IsPlayerInGroup(const FString& eos_id, const FString& group);
+	PERMISSIONS_API bool IsPlayerInGroup(const FString& eos_id, const FString& group);
 
-	ARK_API std::optional<std::string> AddPlayerToGroup(const FString& eos_id, const FString& group);
-	ARK_API std::optional<std::string> RemovePlayerFromGroup(const FString& eos_id, const FString& group);
+	PERMISSIONS_API std::optional<std::string> AddPlayerToGroup(const FString& eos_id, const FString& group);
+	PERMISSIONS_API std::optional<std::string> RemovePlayerFromGroup(const FString& eos_id, const FString& group);
 
-	ARK_API std::optional<std::string> AddPlayerToTimedGroup(const FString& eos_id, const FString& group, int secs, int delaySecs);
-	ARK_API std::optional<std::string> RemovePlayerFromTimedGroup(const FString& eos_id, const FString& group);
+	PERMISSIONS_API std::optional<std::string> AddPlayerToTimedGroup(const FString& eos_id, const FString& group, int secs, int delaySecs);
+	PERMISSIONS_API std::optional<std::string> RemovePlayerFromTimedGroup(const FString& eos_id, const FString& group);
 
-	ARK_API std::optional<std::string> AddGroup(const FString& group);
-	ARK_API std::optional<std::string> RemoveGroup(const FString& group);
+	PERMISSIONS_API std::optional<std::string> AddGroup(const FString& group);
+	PERMISSIONS_API std::optional<std::string> RemoveGroup(const FString& group);
 
-	ARK_API bool IsGroupHasPermission(const FString& group, const FString& permission);
-	ARK_API bool IsPlayerHasPermission(const FString& eos_id, const FString& permission);
+	PERMISSIONS_API bool IsGroupHasPermission(const FString& group, const FString& permission);
+	PERMISSIONS_API bool IsPlayerHasPermission(const FString& eos_id, const FString& permission);
 
-	ARK_API std::optional<std::string> GroupGrantPermission(const FString& group, const FString& permission);
-	ARK_API std::optional<std::string> GroupRevokePermission(const FString& group, const FString& permission);
+	PERMISSIONS_API std::optional<std::string> GroupGrantPermission(const FString& group, const FString& permission);
+	PERMISSIONS_API std::optional<std::string> GroupRevokePermission(const FString& group, const FString& permission);
 
 
-	ARK_API std::optional<std::string> AddTribeToGroup(int tribeId, const FString& group);
-	ARK_API std::optional<std::string> RemoveTribeFromGroup(int tribeId, const FString& group);
+	PERMISSIONS_API std::optional<std::string> AddTribeToGroup(int tribeId, const FString& group);
+	PERMISSIONS_API std::optional<std::string> RemoveTribeFromGroup(int tribeId, const FString& group);
 
-	ARK_API std::optional<std::string> AddTribeToTimedGroup(int tribeId, const FString& group, int secs, int delaySecs);
-	ARK_API std::optional<std::string> RemoveTribeFromTimedGroup(int tribeId, const FString& group);
+	PERMISSIONS_API std::optional<std::string> AddTribeToTimedGroup(int tribeId, const FString& group, int secs, int delaySecs);
+	PERMISSIONS_API std::optional<std::string> RemoveTribeFromTimedGroup(int tribeId, const FString& group);
 
-	ARK_API bool IsTribeInGroup(int tribeId, const FString& group);
-	ARK_API bool IsTribeHasPermission(int tribeId, const FString& permission);
-	ARK_API TArray<FString> GetTribeGroups(int tribeId);
+	PERMISSIONS_API bool IsTribeInGroup(int tribeId, const FString& group);
+	PERMISSIONS_API bool IsTribeHasPermission(int tribeId, const FString& permission);
+	PERMISSIONS_API TArray<FString> GetTribeGroups(int tribeId);
 
-	ARK_API void AddPlayerPermissionCallback(FString CallbackName, bool onlyCheckOnline, bool cacheBySteamId, bool cacheByTribe, const std::function<TArray<FString>(FString*, int*)>& callback);
-	ARK_API void RemovePlayerPermissionCallback(FString CallbackName);
+	PERMISSIONS_API void AddPlayerPermissionCallback(FString CallbackName, bool onlyCheckOnline, bool cacheBySteamId, bool cacheByTribe, const std::function<TArray<FString>(const FString&, int*)>& callback);
+	PERMISSIONS_API void RemovePlayerPermissionCallback(FString CallbackName);
 }
